86cf1f5de540931e481594026adca481eac2e825
Zheng, Lei <realthunder.dev@gmail.com>
2024-03-23 17:20:45 +0800

    App: fix PropertyExpressionEngine::renameExpressions()
    Fixes #974

--- src/App/PropertyExpressionEngine.cpp
+++ src/App/PropertyExpressionEngine.cpp
@@ -895,7 +895,7 @@ void PropertyExpressionEngine::renameExpressions(const std::map<ObjectIdentifier
 
         // Renamed now?
         if (j != canonicalPaths.end())
-            newExpressions[canonicalPath(j->second)] = i->second;
+            newExpressions[j->second] = i->second;
         else
             newExpressions[i->first] = i->second;
     }
