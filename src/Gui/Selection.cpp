b50ff630653428bb716a12777c632a06833ae8f0
Zheng, Lei <realthunder.dev@gmail.com>
2024-08-28 21:02:36 +0800

    Gui: fix recursive selection notification
    Fixes #557
    Fixes #710

--- src/Gui/Selection.cpp
+++ src/Gui/Selection.cpp
@@ -519,7 +519,8 @@ void SelectionSingleton::notify(SelectionChanges &&Chng)
     Base::FlagToggler<bool> flag(Notifying);
     NotificationQueue.push_back(std::move(Chng));
     while(!NotificationQueue.empty()) {
-        const auto &msg = NotificationQueue.front();
+        auto msg = NotificationQueue.front();
+        NotificationQueue.pop_front();
         bool notify;
         switch(msg.Type) {
         case SelectionChanges::AddSelection:
@@ -548,7 +549,6 @@ void SelectionSingleton::notify(SelectionChanges &&Chng)
                 Base::Console().Warning("notify: Unexpected boost exception\n");
             }
         }
-        NotificationQueue.pop_front();
     }
     NotificationRecursion = 0;
 }
